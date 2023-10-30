b259b191794000d41099bca3afdb6ca3997660ad
Brian Mulindi <mulindijrn@gmail.com>
2023-10-30 03:47:08 +0300

    Fix previous commit - Correct empty Transactions.jsx file

---
+++ src/Transactions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Transactions({ date, description, category, amount }) {
+  
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+    </tr>
+  );
+}
+
+export default Transactions;
