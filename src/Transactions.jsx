90e1a5954a7b31721a060b147882615148e7209a
Brian Mulindi <mulindijrn@gmail.com>
2023-10-30 03:50:00 +0300

    Implement transaction filtering based on search term in Transactions component

--- src/Transactions.jsx
+++ src/Transactions.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-function Transactions({ date, description, category, amount }) {
-  
+function Transactions({ date, description, category, amount, searchTerm }) {
+  if (searchTerm && !description.toLowerCase().includes(searchTerm.toLowerCase())) {
+    return null; // Hide the transaction if it doesn't match the search term
+  }
+
   return (
     <tr>
       <td>{date}</td>
